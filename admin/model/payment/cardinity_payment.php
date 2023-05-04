cfb5fe67124737c5db8b1a0dab8b8e6a7cc73168
Shabab Haider Siddique <shabab@cardinity.com>
2023-05-04 19:16:37 +0600

    PHP-553 opencart 4.0.2 https://github.com/opencart/opencart/releases/tag/4.0.2.0 changes applied, added session persistence on db

--- admin/model/payment/cardinity_payment.php
+++ admin/model/payment/cardinity_payment.php
@@ -12,4 +12,38 @@ class CardinityPayment extends \Opencart\System\Engine\Model {
 
 		return $this->config->get('config_subscription_active_status_id');
 	}
+
+	public function install() {
+
+		$this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "cardinity_order`;");
+		$this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "cardinity_session`;");
+
+		$this->createMissingTables();
+	}
+
+	public function uninstall() {
+		$this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "cardinity_order`;");
+		$this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "cardinity_session`;");
+	}
+
+	public function createMissingTables(){
+		$this->db->query("
+			CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "cardinity_order` (
+			`cardinity_order_id` INT(11) NOT NULL AUTO_INCREMENT,
+			`order_id` INT(11) NOT NULL,
+			`payment_id` VARCHAR(255),
+			`payment_status` VARCHAR(255),
+			PRIMARY KEY (`cardinity_order_id`)
+			) ENGINE=MyISAM DEFAULT COLLATE=utf8_general_ci;
+		");
+
+		$this->db->query("
+			CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "cardinity_session` (
+			`cardinity_session_id` INT(11) NOT NULL AUTO_INCREMENT,
+			`session_id` VARCHAR(255) NOT NULL,
+			`session_data` TEXT,
+			PRIMARY KEY (`cardinity_session_id`)
+			) ENGINE=MyISAM DEFAULT COLLATE=utf8_general_ci;
+		");		
+	}
 }
